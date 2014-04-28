78c1fcdddfdea8c69794b4dc7121b0e168c07689
Apoorv Parijat <apoorvparijat@gmail.com>
2014-04-28 16:00:31 +0530

    Now writes data to file

--- crawl-jiit.rb
+++ crawl-jiit.rb
@@ -1,4 +1,6 @@
 require 'mechanize'
+require 'debugger'
+require 'nokogiri'
 
 HOST = 'https://webkiosk.jiit.ac.in/'
 
@@ -8,9 +10,9 @@ ATTENDANCE_URL = HOST + 'StudentFiles/Academic/StudentAttendanceList.jsp'
 params = {
 	'InstCode' => 'JIIT',
 	'UserType' => 'S',
-	'MemberCode' => '11503878',	
-	'DATE1' => '06-11-1994',
-	'Password' => 'utkarsh4$'
+	'MemberCode' => '11503873',	
+	'DATE1' => '15-04-1994',
+	'Password' => 'ujjw@l15'
 }
 
 agent = Mechanize.new
@@ -20,3 +22,20 @@ response = agent.post(LOGIN_URL, params)
 attendance_response = agent.get(ATTENDANCE_URL)
 
 puts attendance_response.body
+
+#debugger
+
+puts '23'
+
+
+dom=Nokogiri::HTML(attendance_response.body)
+dom.css('td').each {|el| puts el.text}
+
+dom.css('td').each do |el| 
+		File.open("11503873.txt", "a") do |file|  
+		file.puts el.text
+	end
+end
+
+
+
